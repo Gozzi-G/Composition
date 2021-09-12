7743da7cd0a677a0649e17243b6a2074a1905f8e
Yury <struck.yury@gmail.com>
2021-09-12 14:09:19 +0300

    Add navigation between fragments

--- app/src/main/java/com/demo/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/demo/composition/domain/entity/GameSettings.kt
@@ -1,11 +1,12 @@
 package com.demo.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-) {
-
-
-}
+): Parcelable
--- app/src/main/java/com/demo/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/demo/composition/presentation/GameFragment.kt
@@ -7,13 +7,23 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.demo.composition.R
 import com.demo.composition.databinding.FragmentGameBinding
+import com.demo.composition.domain.entity.GameResult
+import com.demo.composition.domain.entity.GameSettings
+import com.demo.composition.domain.entity.Level
 import java.lang.RuntimeException
 
 class GameFragment : Fragment() {
 
+    private lateinit var level: Level
+
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +34,53 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1.setOnClickListener {
+            launchGameFinishedFragment(GameResult(
+                true,
+                0,
+                0,
+                GameSettings(
+                    0,
+                    0,
+                    0,
+                    0
+                )
+            ))
+
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
+        }
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+
+        const val NAME = "GameFragment"
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
