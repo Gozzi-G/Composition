5f762f63e5814628617cd48b5752411670336a12
Yury <struck.yury@gmail.com>
2021-09-12 16:38:24 +0300

    Implement GameViewModel

--- app/src/main/java/com/demo/composition/domain/usecases/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/demo/composition/domain/usecases/GenerateQuestionUseCase.kt
@@ -4,7 +4,9 @@ import com.demo.composition.domain.entity.Question
 import com.demo.composition.domain.repository.GameRepository
 
 class GenerateQuestionUseCase(
+
     private val repository: GameRepository
+
 ) {
     operator fun invoke(maxSumValue: Int): Question {
         return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
